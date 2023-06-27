c002798b0bb393be9cd8b6b2e7bc0bec567f6507
DanielMarella <marella.daniel96@gmail.com>
2023-06-27 19:19:12 +0200

    populated several components of main with their own images, texts, and properties

--- src/components/RestaurantMain.vue
+++ src/components/RestaurantMain.vue
@@ -18,38 +18,63 @@
 
         <!-- SECOND COMPONENT -->
         <div class="container">
-            <div class="row mb-5">
+            <div class="row height mb-5">
                 <div class="col-6">
-                    <h1>TESTO</h1>
+                    <p>
+                        HAC TELLUS, FELIS RISUS AT mattis mattis. Eget euismod semper eget tortor, donec amet, blandit. Tristique facilisi faucibus elementum feugiat in nam in feugiat. Ipsum odio etiam duis facilisis amet vulputate.
+                    </p>
+                    <h1>
+                        food is our common ground, a universal experience
+                    </h1>
+                    <p>
+                        Tristique tempus condimentum diam donec. Condimentum ullamcorper sit elementum hendrerit mi nulla in consequat, ut. Metus, nullam scelerisque netus viverra dui.
+                    </p>
+                    <img src="../assets/img/signature.jpg" alt="firma" class="w-25">
                 </div>
-                <div class="col-6">
-                    <h1>IMMAGINE</h1>
+                <div class="col info22 d-flex align-items-end justify-content-center t-white pb-3">
+                    <p>california rolls - $22</p>
                 </div>
             </div>
             <div class="row ">
                 <div class="col-4">
-                    <h1>IMMAGINE</h1>
-                    <h1>TITOLTO</h1>
-                    <h1>TESTO</h1>
+                    <img src="../assets/img/sushi-1.png" alt="">
+                    <h1>the best table in town</h1>
+                    <hr class="w-25">
+                    <p>
+                        Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.
+                    </p>
+                    <button class="button-grey transparent">
+                        explore the menu
+                    </button>
                 </div>
                 <div class="col-4">
-                    <h1>IMMAGINE</h1>
-                    <h1>TITOLTO</h1>
-                    <h1>TESTO</h1>
+                    <img src="../assets/img/sushi-2.png" alt="">
+
+                    <h1>perfect for groups</h1>
+                    <p>
+                        Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet nunc. Orci elementum in aliquet a gravida vivamus aliquam turpis vitae.
+                    </p>
+                    <button class="button-grey transparent">
+                        make a reservation
+                    </button>
                 </div>
                 <div class="col-4">
-                    <h1>IMMAGINE</h1>
-                    <h1>TITOLTO</h1>
-                    <h1>TESTO</h1>
+                    <img src="../assets/img/sushi-3.png" alt="">
+                    <h1>fresh produce everyday</h1>
+                    <p>
+                        Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet nunc. Orci elementum in aliquet a gravida vivamus aliquam turpis vitae.
+                    </p>
+                    <button class="button-grey transparent">
+                        learn more about us
+                    </button>
                 </div>
             </div>
         </div>
 
         <!-- THIRD COMPONENT -->
-        <div class="container ">
+        <div class="container-fluid mb-5 ">
             <div class="row">
-                <div class="col-8">
-                    <h1>IMMAGINE</h1>
+                <div class="col-8 slider32">
                 </div>
                 <div class="col-4 black t-white">
                     <h1>the best table in the town</h1>
@@ -62,46 +87,54 @@
         </div>
 
         <!-- FOURTH COMPONENT -->
-        <div>
-            <h1>
+        <div class="container text-center">
+            <p class="fst-italic fs-1">
                 What critics are saying about us
-            </h1>
+            </p>
+            <hr> 
         </div>
         <div class="container">
             <div class="row mb-5">
-                <div class="col-6 blog height">
+                <div class="col blog">
                 </div>
-                <div class="col-6 black t-white">
-                    <h3>meggy stewart</h3>
-                    <h1>new york times</h1>
-                    <p>
-                        Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed at.Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi.
-                    </p>
-                    <button class="button black t-white">
-                        read full article
-                    </button>
+                <div class="col black t-white">
+                    <div class="container text-start my_margin" >
+                        <h3>meggy stewart</h3>
+                        <h1 class="mb-5">new york times</h1>
+                        <p class="w-50 mb-4">
+                            Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed at.Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi.
+                        </p>
+                        <button class="button black t-white">
+                            read full article
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="row d-flex justify-content-between ">
-                <div class="col-5 info52 t-white ">
-                    <h3>mary maxey</h3>
-                    <h1>the guardian</h1>
-                    <p>
-                        Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed.
-                    </p>
-                    <button class="button transparent t-white">
-                        read full article
-                    </button>
+            <div class="row text-center ">
+                <div class="col article info52 t-white  my_mr">
+                    <div class="container text-start my_margin" >
+                        <h3>mary maxey</h3>
+                        <h1 class="mb-5">the guardian</h1>
+                        <p class="w-50 mb-4">
+                            Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed.
+                        </p>
+                        <button class="button transparent t-white">
+                            read full article
+                        </button>
+                    </div>
                 </div>
-                <div class="col-5 info12 t-white ">
-                    <h3>patrick monroe</h3>
-                    <h1>globe and mail</h1>
-                    <p>
-                        Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt.
-                    </p>
-                    <button class="button transparent t-white">
-                        read full article
-                    </button>
+                <div class="col info12 t-white">
+                    <div class="container text-start my_margin" >
+                        <h3>patrick monroe</h3>
+                        <h1 class="mb-5">globe and mail</h1>
+                        <p class="w-50 mb-4">
+                            Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt.
+                        </p>
+                        <button class="button transparent t-white">
+                            read full article
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -117,56 +150,79 @@
 
         <!--SIXTH COMPONENT -->
         <div class="container">
-            <div class="row ">
-                <div class="col-4">
-                    <h1>IMMAGINE</h1>
-                    <h1>TITOLTO</h1>
-                    <h1>TESTO</h1>
+            <div class="container text-center mb-5">
+                <p class="fst-italic fs-1 ">
+                    Latest news & promotions
+                </p>
+                <hr> 
+            </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <img src="../assets/img/blog92x-400x458.jpg" alt="">
                 </div>
-                <div class="col-4">
-                    <h1>IMMAGINE</h1>
-                    <h1>TITOLTO</h1>
-                    <h1>TESTO</h1>
+                <div class="col mb-3">
+                    <img src="../assets/img/blog72x-400x458.jpg" alt="">
                 </div>
-                <div class="col-4">
-                    <h1>IMMAGINE</h1>
-                    <h1>TITOLTO</h1>
-                    <h1>TESTO</h1>
+                <div class="col mb-3">
+                    <img src="../assets/img/blog82x-400x458.jpg" alt="">
                 </div>
+            </div>
+            <div class="row text-center mb-5">
+                <div class="col">
+                    <h4>the best coffe in town</h4>
+                    <p>
+                        By admin | January 7th, 2020|Categories: News
+                    </p>
+                </div>
+                <div class="col">
+                    <h4>discover our new menu</h4>
+                    <p>
+                        By admin | January 7th, 2020|Categories: News
+                    </p>
+                </div>
+                <div class="col ">
+                    <h4>we now accept square!</h4>
+                    <p>
+                        By admin | January 7th, 2020|Categories: News
+                    </p>
+                </div>
+
             </div>
         </div>
 
         <!-- SEVENTH COMPONENT  -->
-        <div class="container-fluid couple">
-            <div class="row height">
-                <div class="col-8 couple">
+        <div class="container-fluid couple mb-5">
+            <div class="row mb-5">
+                <div class="col couple">
                 </div>
-                <div class="col black t-white">
-                    <h1>takeout now avaiable</h1>
-                    <p>
-                        Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla non arcu moris tortor.
-                    </p>
-                    <button class="button black t-white">
-                        view takeout menu
-                    </button>
+                <div class="col-4 black t-white">
+                    <div class="container w-75 text-start my_margin" >
+                        <h1 class="w-50 mb-5">takeout now avaiable</h1>
+                        <p class="w-50 mb-4">
+                            Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla non arcu moris tortor.
+                        </p>
+                        <button class="button black t-white">
+                            view takeout menu
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
 
         <!-- HEIGHT COMPONENT -->
-        <div class="container">
-            <div class="row ">
-                <div class="col-4 mt-5">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col mt-5">
                     <h1>IMMAGINE</h1>
                     <h1>TITOLTO</h1>
                     <h1>TESTO</h1>
                 </div>
-                <div class="col-4">
+                <div class="col">
                     <h1>IMMAGINE</h1>
                     <h1>TITOLTO</h1>
                     <h1>TESTO</h1>
                 </div>
-                <div class="col-4 mt-5">
+                <div class="col mt-5">
                     <h1>IMMAGINE</h1>
                     <h1>TITOLTO</h1>
                     <h1>TESTO</h1>
@@ -199,52 +255,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    main{
-        border: 3px solid blue;
 
-        .container{
-            padding: 20px 0;
-            margin: 80px auto;
-        }
-        .col-4{
-            border: 2px solid red;
-            height: 700px;
-        }
+    .my_margin{
+        margin: 150px 150px;
+    }
+    .article {
+        height: 750px;
+    }
+
+    main{
 
         .col-5{
-            border: 2px solid red;
             height: 500px;
         }
 
-        .col-6{
-            border: 2px solid red;
-
-        }
-
-
-
-        .col-8{
-            border: 2px solid fuchsia;
-
-        }
-
         .col-12{
-            border: 2px solid fuchsia;
             height: 800px;
         }
         
         
     }
+    .my_mr{
+            margin-right: 50px;
+        }
 
-    .height{
-        height: 600px;
-    }
-
-    .row{
-        border: 3px solid cyan;
-    }
-
-    
     .my_fs{
         font-size: 90px;
         text-transform: uppercase;  
@@ -273,25 +307,49 @@ export default {
     background-position: center;
 }
 
+.blog82{
+    background-image: url(../assets/img/blog82x-1200x1375.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
+.blog92{
+    background-image: url(../assets/img/blog92x-1200x1375.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
 .info12 {
     background-image:
     linear-gradient(to bottom, rgb(0, 0, 0), rgba(255, 255, 255, 0)),
     url(../assets/img/info12x.jpg);
     background-size: cover;
     color: white;
-    padding: 20px;
 }
+
+.info22{
+    background-image: url(../assets/img/info22x.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+
 .info52{
     background-image:
     linear-gradient(to bottom, rgb(0, 0, 0), rgba(255, 255, 255, 0)),
     url(../assets/img/info52x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
-    background-position: center;
+
+}
+
+.slider32{
+    background-image: url(../assets/img/slider32x.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
 }
 
 
-
 </style>
-
-<!-- rl(); -->
