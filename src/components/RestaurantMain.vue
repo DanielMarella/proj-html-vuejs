1760bec86dae8ebcdca62c185630781f384d7461
DanielMarella <marella.daniel96@gmail.com>
2023-06-28 15:31:11 +0200

    add all new component in my main

--- src/components/RestaurantMain.vue
+++ src/components/RestaurantMain.vue
@@ -1,334 +1,64 @@
 <template>
     <main>
-        <!-- FIRST COMPONENT -->
-        <div class="container ">
-            <div class="row">
-                <div class="col-5">
-                    <h1 class='my_fs'>fine dining experience</h1>
-                    <p>Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla. Sit tempor a et nisl, ac felis.</p>
-                    <button class="button black t-white">
-                        explore the menu
-                    </button>
-                </div>
-                <div class="col-7">
-                    <h1>IMMAGINE</h1>
-                </div>
-            </div>
-        </div>
 
         <!-- SECOND COMPONENT -->
-        <div class="container mb-5">
-            <div class="row height mb-5">
-                <div class="col-6">
-                    <p>
-                        HAC TELLUS, FELIS RISUS AT mattis mattis. Eget euismod semper eget tortor, donec amet, blandit. Tristique facilisi faucibus elementum feugiat in nam in feugiat. Ipsum odio etiam duis facilisis amet vulputate.
-                    </p>
-                    <h1>
-                        food is our common ground, a universal experience
-                    </h1>
-                    <p>
-                        Tristique tempus condimentum diam donec. Condimentum ullamcorper sit elementum hendrerit mi nulla in consequat, ut. Metus, nullam scelerisque netus viverra dui.
-                    </p>
-                    <img src="../assets/img/signature.jpg" alt="firma" class="w-25">
-                </div>
-                <div class="col info22 d-flex align-items-end justify-content-center t-white pb-3">
-                    <p>california rolls - $22</p>
-                </div>
-            </div>
-            <div class="row ">
-                <div class="col-4">
-                    <img src="../assets/img/sushi-1.png" alt="">
-                    <h1>the best table in town</h1>
-                    <hr class="w-25">
-                    <p>
-                        Sed aenean egestas ut aliquam turpis mauris, molestie. Vitae tellus tempor sem id tempus neque, tellus turpis turpis. Morbi tortor id gravida aliquet.
-                    </p>
-                    <button class="button-grey transparent">
-                        explore the menu
-                    </button>
-                </div>
-                <div class="col-4">
-                    <img src="../assets/img/sushi-2.png" alt="">
-
-                    <h1>perfect for groups</h1>
-                    <p>
-                        Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet nunc. Orci elementum in aliquet a gravida vivamus aliquam turpis vitae.
-                    </p>
-                    <button class="button-grey transparent">
-                        make a reservation
-                    </button>
-                </div>
-                <div class="col-4">
-                    <img src="../assets/img/sushi-3.png" alt="">
-                    <h1>fresh produce everyday</h1>
-                    <p>
-                        Quam eu proin sit massa condimentum. Volutpat non pulvinar aliquet nunc. Orci elementum in aliquet a gravida vivamus aliquam turpis vitae.
-                    </p>
-                    <button class="button-grey transparent">
-                        learn more about us
-                    </button>
-                </div>
-            </div>
-        </div>
+        <SecondComponent />
 
         <!-- THIRD COMPONENT -->
-        <div class="container-fluid mb-5">
-            <div class="row">
-                <div class="col-8 slider32">
-                </div>
-                <div class="col-4 black t-white">
-                    <div class="container w-75 text-start my_margin" >
-                        <h1 class="w-75 mb-5">the best table in town</h1>
-                        <p class="w-50 mb-4">
-                            Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla.
-                        </p>
-                        <button class="button black t-white">
-                            explore the menu
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ThirdComponent />
 
         <!-- FOURTH COMPONENT -->
-        <div class="container text-center">
-            <p class="fst-italic fs-1">
-                What critics are saying about us
-            </p>
-            <hr> 
-        </div>
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col blog">
-                </div>
-                <div class="col black t-white">
-                    <div class="container text-start my_margin" >
-                        <h3>meggy stewart</h3>
-                        <h1 class="mb-5">new york times</h1>
-                        <p class="w-50 mb-4">
-                            Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed at.Mmalesuada ullamcorper amet. Pretium pretium dignissim nisi.
-                        </p>
-                        <button class="button black t-white">
-                            read full article
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div class="row text-center ">
-                <div class="col article info52 t-white  my_mr">
-                    <div class="container text-start my_margin" >
-                        <h3>mary maxey</h3>
-                        <h1 class="mb-5">the guardian</h1>
-                        <p class="w-50 mb-4">
-                            Non arcu mauris tortor ultrices mollis tellus euismod fermentum. Habitant amet tincidunt id sapien accumsan sed.
-                        </p>
-                        <button class="button transparent t-white">
-                            read full article
-                        </button>
-                    </div>
-                </div>
-                <div class="col info12 t-white">
-                    <div class="container text-start my_margin" >
-                        <h3>patrick monroe</h3>
-                        <h1 class="mb-5">globe and mail</h1>
-                        <p class="w-50 mb-4">
-                            Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt.
-                        </p>
-                        <button class="button transparent t-white">
-                            read full article
-                        </button>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+        <FourthComponent />
 
         <!-- FIFTH COMPONENT -->
-        <div class="container-fluid divider my-5 p-5">
-            <div class="row mt-5 p-5">
-                <div class="col mt-5 pt-5 d-flex align-items-end justify-content-center t-white pb-3">
-                    <h4 class="mt-5 pt-5">kung pao chicken - $32</h4>
-                </div>
-            </div>
-        </div>
+        <FifthComponent />
 
         <!--SIXTH COMPONENT -->
-        <div class="container">
-            <div class="container text-center mb-5">
-                <p class="fst-italic fs-1 ">
-                    Latest news & promotions
-                </p>
-                <hr> 
-            </div>
-            <div class="row">
-                <div class="col mb-3">
-                    <img src="../assets/img/blog92x-400x458.jpg" alt="">
-                </div>
-                <div class="col mb-3">
-                    <img src="../assets/img/blog72x-400x458.jpg" alt="">
-                </div>
-                <div class="col mb-3">
-                    <img src="../assets/img/blog82x-400x458.jpg" alt="">
-                </div>
-            </div>
-            <div class="row text-center mb-5">
-                <div class="col">
-                    <h4>the best coffe in town</h4>
-                    <p>
-                        By admin | January 7th, 2020|Categories: News
-                    </p>
-                </div>
-                <div class="col">
-                    <h4>discover our new menu</h4>
-                    <p>
-                        By admin | January 7th, 2020|Categories: News
-                    </p>
-                </div>
-                <div class="col ">
-                    <h4>we now accept square!</h4>
-                    <p>
-                        By admin | January 7th, 2020|Categories: News
-                    </p>
-                </div>
-
-            </div>
-        </div>
+        <SixthComponent />
 
         <!-- SEVENTH COMPONENT  -->
-        <div class="container-fluid couple mb-5">
-            <div class="row mb-5">
-                <div class="col couple">
-                </div>
-                <div class="col-4 black t-white">
-                    <div class="container w-75 text-start my_margin" >
-                        <h1 class="w-50 mb-5">takeout now avaiable</h1>
-                        <p class="w-50 mb-4">
-                            Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla non arcu moris tortor.
-                        </p>
-                        <button class="button black t-white">
-                            view takeout menu
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <SeventhComponent />
 
         <!-- HEIGHT COMPONENT -->
-        <div class="container mt-5 mb-5">
-            <div class="row">
-                <div class="col grey mt-5">
-                    <div class="container w-75 mt-5">
-                        <h1 class="mb-5 mt-5 text-start fs-2">hors d'oeuvres</h1>
-
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">Ahi salmon nigiri <span class="fs-6">$48</span></h3>
-                        <p class="mb-5">
-                            Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit lacinia.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">umi masu salad<span class="fs-6">$21</span></h3>
-                        <p class="mb-5">
-                            Mollis ornare sit sapien, sodales. Cursus duis proin semper quisque.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">temaki with  crab <span class="fs-6">$32</span></h3>
-                        <p class="mb-5">
-                            Ac vel, risus ornare senectus placerat duis amet dictumst tellus.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">california rolls<span class="fs-6">$22</span></h3>
-                        <p class="mb-5">
-                            Ac vel, risus ornare senectus placerat duis amet dictumst tellus
-                        </p>
-                    </div>
-                </div>
-                <div class="col menu h-100">
-                    <div class="container w-75 mt-5">
-                        <h1 class="mb-5 mt-5 text-start fs-2">main course</h1>
-
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">braised abalone<span class="fs-6">$52</span></h3>
-                        <p class="mb-5">
-                            Mollis ornare sit sapien, sodales. Cursus duis proin semper quisque.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">twice cooked pork<span class="fs-6">$21</span></h3>
-                        <p class="mb-5">
-                            Mollis ornare sit sapien, sodales. Cursus duis proin semper quisque.
-                        </p>
-
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">kung pao chicken <span class="fs-6">$32</span></h3>
-                        <p class="mb-5">
-                            Quis duis sit dictum aliquam a velit enim. Tellus in sit augue aliquam.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">char siu & sushis <span class="fs-6">$48</span></h3>
-                        <p class="mb-5">
-                            Feugiat rhoncus ipsum tristique purus diam, et dolor molestie.
-                        </p>
-                    </div>
-                </div>
-                <div class="col grey mt-5">
-                    <div class="container w-75 mt-5">
-                        <h1 class="mb-5 mt-5 text-start fs-2">desserts</h1>
-
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">chocolate ball cake<span class="fs-6">$13</span></h3> 
-                        
-                        <p class="mb-5">
-                            In pellentesque in felis massa vehicula. In sed euismod ut.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">lime pie with crust<span class="fs-6">$14</span></h3>
-                        <p class="mb-5">
-                            Mollis ornare sit sapien, sodales. Cursus duis proin semper quisque.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">rasberry pear cake<span class="fs-6">$6</span></h3>
-                        <p class="mb-5">
-                            Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit.
-                        </p>
-    
-                        <h3 class="fs-5 mb-4 d-flex justify-content-between align-items-baseline">café au lait<span class="fs-6">$48</span></h3>
-                        <p class="mb-5">
-                            Eget vulputate vitae quis rutrum blandit sed. Quam nulla sit.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <HeightComponent />
 
         <!-- NINTH COMPONENT -->
-        <div class="container-fluid divider-hero p-5">
-            <div class="container">
-                <div class="row">
-                    <div class="col-4 m-5 black t-white">
-                        <div class="container text-start m-5" >
-                            <h1 class="w-50 mb-5 d-flex">braised abalone <span class="fs-6 mx-5 mt-2">$52</span></h1>
-                            <hr class="m-0 mb-5">
-                            <p class="w-50 mb-4">
-                                Pellentesque vitae viverra risus, sagittis. Venenatis ridiculus scelerisque nisi in urna nulla non arcu moris tortor.
-                            </p>
-                            <button class="button black t-white">
-                                view takeout menu
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <NinthComponent />
     </main>
 </template>
 
 <script>
+import SecondComponent from './restaurantMain_children/SecondComponent.vue';
+import ThirdComponent from './restaurantMain_children/ThirdComponent.vue';
+import FourthComponent from './restaurantMain_children/FourthComponent.vue';
+import FifthComponent from './restaurantMain_children/FifthComponent.vue';
+import SixthComponent from './restaurantMain_children/SixthComponent.vue';
+import SeventhComponent from './restaurantMain_children/SeventhComponent.vue';
+import HeightComponent from './restaurantMain_children/HeightComponent.vue';
+import NinthComponent from './restaurantMain_children/NinthComponent.vue';
+
+
 
 export default {
-    name : 'RestaurantMain'
+    name : 'RestaurantMain',
+
+    components : {
+        SecondComponent, 
+        ThirdComponent,
+        FourthComponent,
+        FifthComponent,
+        SixthComponent,
+        SeventhComponent,
+        HeightComponent,
+        NinthComponent
+    }
+    
+    
 }
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 
     .my_margin{
         margin: 150px 150px;
@@ -361,41 +91,41 @@ export default {
 
     // background-image
 .couple{
-    background-image: url(../assets/img/slider72x-scaled.jpg);
+    background-image: url(/img/slider72x-scaled.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
 }
 
 .divider{
-    background-image: url(../assets/img/page52x.jpg);
+    background-image: url(/img/page52x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
 }
 
 .divider-hero{
-    background-image: url(../assets/img/hero42x.jpg);
+    background-image: url(/img/hero42x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
 }
 .blog{
-    background-image: url(../assets/img/blog72x-1200x1375.jpg);
+    background-image: url(/img/blog72x-1200x1375.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
 }
 
 .blog82{
-    background-image: url(../assets/img/blog82x-1200x1375.jpg);
+    background-image: url(/img/blog82x-1200x1375.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
 }
 
 .blog92{
-    background-image: url(../assets/img/blog92x-1200x1375.jpg);
+    background-image: url(/img/blog92x-1200x1375.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
@@ -404,21 +134,20 @@ export default {
 .menu {
     background-image:
     linear-gradient(to bottom, rgb(0, 0, 0), rgba(0, 0, 0, 0.561)),
-    url(../assets/img/misc22x.jpg);
+    url(/img/misc22x.jpg);
     background-size: cover;
     color: white;
 }
-
 .info12 {
     background-image:
     linear-gradient(to bottom, rgb(0, 0, 0), rgba(255, 255, 255, 0)),
-    url(../assets/img/info12x.jpg);
+    url(/img/info12x.jpg);
     background-size: cover;
     color: white;
 }
 
 .info22{
-    background-image: url(../assets/img/info22x.jpg);
+    background-image: url(/img/info22x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center;
@@ -427,17 +156,16 @@ export default {
 .info52{
     background-image:
     linear-gradient(to bottom, rgb(0, 0, 0), rgba(255, 255, 255, 0)),
-    url(../assets/img/info52x.jpg);
+    url(/img/info52x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
 
 }
 
 .slider32{
-    background-image: url(../assets/img/slider32x.jpg);
+    background-image: url(/img/slider32x.jpg);
     background-size: cover;
     background-repeat: no-repeat;
 }
-
 
 </style>
