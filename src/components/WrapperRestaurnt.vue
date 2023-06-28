297427ca1e883ad15d9e3233d59681544a96cab2
DanielMarella <marella.daniel96@gmail.com>
2023-06-28 15:31:20 +0200

    create a wrapper component

---
+++ src/components/WrapperRestaurnt.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="wrapper">
+        <FirstComponent />
+    </div>
+</template>
+
+<script>
+
+import FirstComponent from './restaurantMain_children/FirstComponent.vue';
+
+export default {
+    name : 'WrapperRestaurant',
+    components : {
+        FirstComponent
+    }
+}
+</script>
+<style lang="scss" >
+    
+
+    .wrapper{
+        background-image: url(../assets/img/slider52x.jpg);
+    }
+
+</style>
