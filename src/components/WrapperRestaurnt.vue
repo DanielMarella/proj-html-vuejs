d8e35dff0a25fa1560fccd60c76754922c18a4bd
DanielMarella <marella.daniel96@gmail.com>
2023-06-28 16:44:47 +0200

    adding navbar and props

--- src/components/WrapperRestaurnt.vue
+++ src/components/WrapperRestaurnt.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="wrapper">
         <div class="container-fluid">
-
             <div class="row mb-5 ">
-                <div class="col w-50 mb-5 black">
+                <div class="col d-flex flex-column  w-50 mb-5 black">
+                    <div class="col h-nav m-5 d-flex align-items-center ">
+                        <img src="/img/logo-restaurant-2x-300x58.png" alt="restaurant logo" class="">
+                    </div>
                     <FirstComponent />
                 </div>
-                <div class="col px-0 mb-5">
-                    <img src="/img/slider52x.jpg" alt="">
+                <div class="col backgroud-restaurant px-0 mb-5">
+                    <div class="nav m-5 d-flex justify-content-center align-items-center">
+                        <RestaurantNav v-for="link in links" 
+                        :navElement = "link"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
@@ -15,24 +21,70 @@
 </template>
 
 <script>
-
+import RestaurantNav from './restaurantHeader_children/RestaurantNav.vue'
 import FirstComponent from './restaurantMain_children/FirstComponent.vue';
 
 export default {
     name : 'WrapperRestaurant',
     components : {
-        FirstComponent
+        FirstComponent,
+        RestaurantNav,
+
+    },
+
+    data() {
+    return {
+
+    links : [
+        {
+                text : 'home',
+            },
+
+            {
+                text : 'history',
+            },
+
+            {
+                text : 'team',
+            },
+
+            {
+                text : 'menu',
+            },
+
+            {
+                text : 'takeout',
+            },
+            
+            {
+                text : 'bulletin'
+            },
+
+            {
+                text : 'reservation'
+            },
+        ]
     }
+  },
+
+
 }
 </script>
 <style lang="scss" >
     
 
     .wrapper{
-        img{
-            width: 100%;
-            height: 100%;
+
+        .h-nav{
+            height: 100px;
         }
+
+        .backgroud-restaurant{
+            background-image: url(/img/slider52x.jpg/);
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
     }
 
 </style>
---
+++ src/components/restaurantHeader_children/RestaurantNav.vue
@@ -0,0 +1,30 @@
+<template>
+        <ul>
+            <li>
+                {{ navElement.text }}
+            </li>
+        </ul>
+</template>
+
+
+<script>
+
+export default {
+    name : 'RestaurantNav',
+        props : {
+            navElement : String,
+        }
+}
+</script>
+
+
+<style lang="scss">
+    ul{
+        color: white;
+        list-style-type: none;
+    }
+
+    li{
+        text-transform: uppercase;
+    }
+</style>
