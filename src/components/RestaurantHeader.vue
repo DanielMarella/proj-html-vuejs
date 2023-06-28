329fb386144b4b32f4580dea99f09fa7ce6b9178
DanielMarella <marella.daniel96@gmail.com>
2023-06-28 16:42:33 +0200

    nothing change

--- src/components/RestaurantHeader.vue
+++ src/components/RestaurantHeader.vue
@@ -6,12 +6,11 @@
 
 <script>
 import WrapperRestaurnt from './WrapperRestaurnt.vue';
-
 export default {
 
     name: "RestaurantHeader",
     components: {
-        WrapperRestaurnt 
+        WrapperRestaurnt,
     }
 }
 
